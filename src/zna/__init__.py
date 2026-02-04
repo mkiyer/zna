1f19bfcdc39d998d3abfd20a9fbc53bbc67f0d66
mkiyer <matthew.iyer@gmail.com>
2026-02-04 11:38:32 -0500

    update to 0.1.5

--- src/zna/__init__.py
+++ src/zna/__init__.py
@@ -1,5 +1,5 @@
 from .core import ZnaHeader, ZnaWriter, ZnaReader, read_zna, write_zna, ZnaHeaderFlags, ZnaRecordFlags, reverse_complement
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 __all__ = ["ZnaHeader", "ZnaWriter", "ZnaReader", "read_zna", "write_zna", "ZnaHeaderFlags", "ZnaRecordFlags", "reverse_complement", "__version__"]
